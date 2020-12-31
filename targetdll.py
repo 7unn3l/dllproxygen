c5c3ec5c1cb605c568c8581863254bca7b1516ba
Sgt Supergeil <maxrademacher02@gmail.com>
2020-12-31 15:04:56 +0100

    added verbose output to targetdll

--- targetdll.py
+++ targetdll.py
@@ -4,9 +4,14 @@ from os.path import basename
 
 class Targetdll():
     def __init__(self,dllpath):
+        print(f'parsing {dllpath}')
         self.dllpath = dllpath
-        self.pe = PE(self.dllpath)
-        self.pe.parse_data_directories()
+        try:
+            self.pe = PE(self.dllpath)
+            self.pe.parse_data_directories()
+        except Exception as e:
+            print(f'could not parse dll: {e}')
+            exit(-1)
 
     def get_arch(self):
         return 32 if self.pe.FILE_HEADER.Machine == 0x014C else 64 
