5923aa2d854c23783d3a816adabfc71972f9e467
Sgt Supergeil <maxrademacher02@gmail.com>
2020-12-31 15:35:45 +0100

    added _gen_fww_exportlist and fixed path bug

--- project.py
+++ project.py
@@ -14,6 +14,7 @@ class Project:
         print(f"outputpath  : {self.outputpath}\n")
 
         self._chk_paths()
+        self._gen_fwd_exportlist()
     
     def _chk_paths(self):
 
@@ -22,12 +23,38 @@ class Project:
             exit(-1)
 
         pdir = dirname(self.outputpath)
-        if not exists(pdir):
+        if not exists(pdir) and pdir:
             print(f'parent directory {pdir} does not exist. aborting.')
             exit(-1)
+
+    def _gen_fwd_exportlist(self):   
+        print('generating linker comments for export forwarding...')
+        fcache = self.proxy_functions.copy()
+        out = ''
+        e = 0
+        bname = self.dll.get_basename()
+
+        for exp,_ord in self.dll.get_exports():
+
+            if exp in self.proxy_functions:
+                fcache.remove(exp)
+                out += f'//#pragma comment(linker,"/export:{exp}={bname}.orig.{exp},@{_ord}")\n'
+            else:
+                out += f'#pragma comment(linker,"/export:{exp}={bname}.orig.{exp},@{_ord}")\n'
+            e += 1
+        
+        print(f'read {e} exports total')
+        
+        
+        for fname in fcache:
+            print(f'WARNING: {fname} function not found in dll exports')
+
+        return out
 
     def _gen_src_files(self):
         pass
     
     def generate(self):
         pass
+
+    pass
